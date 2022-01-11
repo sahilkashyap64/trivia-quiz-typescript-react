32cc4ff8e3958a14c83efc8c73c31f3e95838a1b
sahilkashyap64 <sahilkashyap64@gmail.com>
2022-01-11 08:02:02 +0530

    get random question api added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,75 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios, { CancelTokenSource } from 'axios';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup'; 
+type UserSubmitForm = {
+  useranswer: string;
+};
 
-function App() {
+interface IQuestion {
+  id: number;
+  value?: number;
+  answer: string;
+  question: string;
+}
+const defaultPosts: IQuestion[] = [];
+const App: React.FC = () => {
+  const [questions, setQuestions]: [IQuestion[], (questions: IQuestion[]) => void] = React.useState(
+    defaultPosts
+  );
+
+  const [loading, setLoading]: [
+    boolean,
+    (loading: boolean) => void
+  ] = React.useState<boolean>(true);
+
+  const [error, setError]: [string, (error: string) => void] = React.useState(
+    ''
+  );
+
+  const cancelToken = axios.CancelToken; //create cancel token
+  const [cancelTokenSource, setCancelTokenSource]: [
+    CancelTokenSource,
+    (cancelTokenSource: CancelTokenSource) => void
+  ] = React.useState(cancelToken.source());
+
+  const handleCancelClick = () => {
+    if (cancelTokenSource) {
+      cancelTokenSource.cancel('User cancelled operation');
+    }
+  };
+  const retrieveRandomQuestion = () => {
+    axios
+      .get<IQuestion[]>('https://jservice.io/api/random', {
+        cancelToken: cancelTokenSource.token,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        timeout: 10000,
+      })
+      .then((response) => {
+        setQuestions(response.data);
+        setLoading(false);
+      })
+      .catch((ex) => {
+        let error = axios.isCancel(ex)
+          ? 'Request Cancelled'
+          : ex.code === 'ECONNABORTED'
+          ? 'A timeout has occurred'
+          : ex.response.status === 404
+          ? 'Resource Not Found'
+          : 'An unexpected error has occurred';
+
+        setError(error);
+        setLoading(false);
+      });
+  }
+  React.useEffect(() => {
+    retrieveRandomQuestion();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
